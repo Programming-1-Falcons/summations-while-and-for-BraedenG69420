2bb014697fb7aaf845bb1a0cd94ec8509485ee36
Braeden Green <greenbra015@alpinesd.org>
2024-10-22 11:55:55 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,10 +1,12 @@
 #while summation code following directions from Readme
-user_num= int(input("input number:"))
+user_num= int(input())
 
 total=0
-
+biggie=0
 for i in range(1, user_num+1):
 
-  total = total + 1
+  total = total + i
 
-  print(total)
+print(total)
+biggie=biggie+total
+print(biggie)
