3e6d997230313cf20bc98b277b3765ea62818ada
Braeden Green <greenbra015@alpinesd.org>
2024-10-22 11:51:31 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -3,10 +3,9 @@ user_num=int(input("What is your number?"))
 loopedamount=0
 
 total=0
+
 while loopedamount<=user_num:
 
-        print(loopedamount)
-
-        loopedamount = loopedamount +1
         total=total+loopedamount
-        print("Your total is"+str(total))
+        loopedamount = loopedamount+1
+print("Your total is"+str(total))
